0c11fc68a82b6aa244366329cfbdfa973c8dbc53
Tebloev Stanislav <stas.tebloev@yandex.com>
2019-10-27 22:57:18 +0300

    initial

---
+++ finder.cpp
@@ -0,0 +1,6 @@
+#include "finder.h"
+
+finder::finder()
+{
+
+}
---
+++ finder.h
@@ -0,0 +1,17 @@
+#ifndef FINDER_H
+#define FINDER_H
+
+#include <QObject>
+
+class finder : public QObject
+{
+    Q_OBJECT
+public:
+    finder();
+
+signals:
+
+public slots:
+};
+
+#endif // FINDER_H
